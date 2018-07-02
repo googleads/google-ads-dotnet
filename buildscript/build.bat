da3d6412f4a516e88d00b5406472b03da0923df0
AnashOommen <adwordsapiadvisor+anash@google.com>
2018-07-02 16:03:06 -0400

    Make the build script robust to  working directory. Fail the build if any of the dotnet build commands fail.

--- buildscript/build.bat
+++ buildscript/build.bat
@@ -1,17 +1,27 @@
-@echo off
+::@echo off
+
+:: Save the path of the build script.
+set BuildScriptPath=%~dp0
+
+:: Strip trailing backslash.
+set RootPath=%BuildScriptPath:~0,-1%\..
+
 echo Build the library.
 echo ==================
 
-dotnet build ..\src\Google.Ads.GoogleAds.csproj
+dotnet build "%RootPath%\src\Google.Ads.GoogleAds.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo Build the examples.
 echo ===================
-dotnet build ..\examples\Services\Google.Ads.GoogleAds.Examples.csproj
+dotnet build "%RootPath%\examples\Services\Google.Ads.GoogleAds.Examples.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 echo Run the tests.
 echo ==============
-dotnet build ..\tests\Google.Ads.GoogleAds.Tests.csproj
-@echo on
+dotnet build "%RootPath%\tests\Google.Ads.GoogleAds.Tests.csproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-exit /b %ERRORLEVEL%
+@echo on
+exit /b 0
 
