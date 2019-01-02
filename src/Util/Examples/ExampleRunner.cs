599b8d1cf6c1e96f6456741feec6bfc4eaa6075c
AnashOommen <adwordsapiadvisor+anash@google.com>
2019-01-02 15:50:48 -0500

    Add support for command line parsing and execution of code examples.

---
+++ src/Util/Examples/ExampleRunner.cs
@@ -0,0 +1,167 @@
+﻿// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Ads.GoogleAds.Lib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using SystemType = System.Type;
+
+namespace Google.Ads.GoogleAds.Examples
+{
+    /// <summary>
+    /// Helper class for running code examples.
+    /// </summary>
+    public class ExampleRunner
+    {
+        /// <summary>
+        /// A map to hold the code examples to be executed.
+        /// </summary>
+        private static SortedDictionary<string, ExampleBase> codeExampleMap =
+            new SortedDictionary<string, ExampleBase>(StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Finds the code examples.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        public void LoadCodeExamples(Assembly assembly)
+        {
+            SystemType[] types = assembly.GetTypes();
+
+            foreach (SystemType type in types)
+            {
+                if (type.IsSubclassOf(typeof(ExampleBase)))
+                {
+                    ExampleBase example = (ExampleBase) Activator.CreateInstance(type);
+                    codeExampleMap.Add(example.Name, example);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Prints the list of available code examples.
+        /// </summary>
+        public void PrintAvailableExamples()
+        {
+            foreach (ExampleBase codeExample in codeExampleMap.Values)
+            {
+                Console.WriteLine("{0} : {1}", codeExample.Name, codeExample.Description);
+            }
+        }
+
+        /// <summary>
+        /// Runs the specified example name.
+        /// </summary>
+        /// <param name="exampleName">Name of the example.</param>
+        /// <param name="session">The session.</param>
+        /// <param name="args">The arguments.</param>
+        /// <exception cref="KeyNotFoundException">Thrown if the example with the specified name
+        /// is not found.</exception>
+        public void Run(string exampleName, GoogleAdsClient session, IEnumerable<string> args)
+        {
+            ExampleBase codeExample = null;
+
+            if (!codeExampleMap.TryGetValue(exampleName, out codeExample))
+            {
+                throw new KeyNotFoundException($"Example not found: {codeExample}");
+            }
+
+            Console.WriteLine(codeExample.Description);
+            Flag[] flags = GetFlagsFromRunMethodSignature(codeExample);
+            if (flags.Length > 0)
+            {
+                Flags.Parse(args, flags);
+            }
+
+            object[] paramValues = BuildExampleParameterList(session, flags);
+            MethodInfo runMethod = GetRunMethod(codeExample);
+            try
+            {
+                runMethod.Invoke(codeExample, paramValues);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An exception occurred while running this code example. {0}",
+                    ExampleUtilities.FormatException(e));
+            }
+            finally
+            {
+                Console.WriteLine("Press [Enter] to continue");
+                Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the specified type is Nullable.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>True, if the type is nullable, false otherwise.</returns>
+        private static bool IsNullable(SystemType type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+
+        /// <summary>
+        /// Builds the parameter list for running a code example.
+        /// </summary>
+        /// <param name="session">The session.</param>
+        /// <param name="flags">The flags.</param>
+        /// <returns>The list of parameters for running the code example.</returns>
+        private static object[] BuildExampleParameterList(GoogleAdsClient session, Flag[] flags)
+        {
+            List<object> paramValues = new List<object>() { session };
+            foreach (Flag flag in flags)
+            {
+                paramValues.Add(flag.IsRepeatable ? flag.Values : flag.Value);
+            }
+            return paramValues.ToArray();
+        }
+
+        /// <summary>
+        /// Gets the Run method for a code example.
+        /// </summary>
+        /// <param name="codeExample">The code example.</param>
+        /// <returns>The <see cref="MethodInfo"/> object for the <code>Run</code> method.</returns>
+        private static MethodInfo GetRunMethod(ExampleBase codeExample)
+        {
+            return codeExample.GetType().GetMethod("Run");
+        }
+
+        /// <summary>
+        /// Gets the flags from <code>Run</code> method signature of a code example.
+        /// </summary>
+        /// <param name="codeExample">The code example.</param>
+        /// <returns>A list of flags for running this code example.</returns>
+        private static Flag[] GetFlagsFromRunMethodSignature(ExampleBase codeExample)
+        {
+            // Get the Run method.
+            MethodInfo runMethod = GetRunMethod(codeExample);
+            if (runMethod == null)
+            {
+                throw new MissingMethodException(
+                    $"Run method is missing in the example: {codeExample.Name}.");
+            }
+            List<Flag> retval = new List<Flag>();
+
+            // Skip one parameter, since the first parameter is the GoogleAdsClient.
+            foreach(ParameterInfo parameterInfo in runMethod.GetParameters().Skip(1))
+            {
+                retval.Add(Flags.FromParameterInfo(parameterInfo));
+            }
+            return retval.ToArray();
+        }
+    }
+}
---
+++ tests/Util/Examples/ExampleRunnerTest.cs
@@ -0,0 +1,170 @@
+﻿// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Ads.GoogleAds.Examples;
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.Tests.Util.Examples.V0;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Google.Ads.GoogleAds.Tests.Util.Examples
+{
+    /// <summary>
+    /// UnitTests for <see cref="CollectionUtilitiesTest"/> class.
+    /// </summary>
+    [TestFixture]
+    public class ExampleRunnerTest
+    {
+        private ExampleRunner runner;
+
+        /// <summary>
+        /// Inits this instance.
+        /// </summary>
+        [SetUp]
+        public void Init()
+        {
+            runner = new ExampleRunner();
+            runner.LoadCodeExamples(this.GetType().Assembly);
+        }
+
+        /// <summary>
+        /// Test for <see cref="ExampleRunner.PrintAvailableExamples"/>
+        /// </summary>
+        [Test]
+        public void TestPrintAvailableExamples()
+        {
+            CaptureConsoleIOAndExecute(
+                delegate ()
+                {
+                    runner.PrintAvailableExamples();
+                },
+                delegate (string output)
+                {
+                    string[] outputs = output.Split(new char[] { '\r', '\n' },
+                        StringSplitOptions.RemoveEmptyEntries);
+                    string[] expectedOutputs = new string[] {
+                        $"{TestExample.TEST_EXAMPLE_NAME} : {TestExample.TEST_EXAMPLE_DESCRIPTION}",
+                        $"{NoRunTestExample.TEST_EXAMPLE_NAME} : {NoRunTestExample.TEST_EXAMPLE_DESCRIPTION}"
+                    };
+                    Assert.That(outputs, Is.EquivalentTo(expectedOutputs));
+                }
+            );
+        }
+
+        /// <summary>
+        /// Test for <see cref="ExampleRunner.Run(string, GoogleAdsClient, IEnumerable{string})"/>
+        /// </summary>
+        [Test]
+        public void TestRun()
+        {
+            GoogleAdsClient client = new GoogleAdsClient();
+
+            // Verify that you can run an example with the right set of arguments.
+            CaptureConsoleIOAndExecute(
+                delegate ()
+                {
+                    runner.Run(TestExample.TEST_EXAMPLE_NAME, client, new string[] {
+                        $"--{TestExample.STRING_ARGS_NAME}={TestExample.STRING_ARG1}",
+                        $"--{TestExample.STRING_ARGS_NAME}={TestExample.STRING_ARG2}",
+                        $"--{TestExample.LONG_ARGS_NAME}={TestExample.LONG_ARG1}",
+                        $"--{TestExample.LONG_ARGS_NAME}={TestExample.LONG_ARG2}",
+                        $"--{TestExample.LONG_ARGS_NAME}={TestExample.LONG_ARG3}",
+                        $"--{TestExample.SINGLE_STRING_ARG_NAME}={TestExample.SINGLE_STRING_ARG}",
+                        $"--{TestExample.SINGLE_LONG_ARG_NAME}={TestExample.SINGLE_LONG_ARG}"
+                    });
+                }, null
+            );
+
+            // Verify that a KeyNotFoundException is thrown if the specified example
+            // is not found.
+            CaptureConsoleIOAndExecute(
+                delegate ()
+                {
+                    Assert.Throws(typeof(KeyNotFoundException),
+                        delegate ()
+                        {
+                            runner.Run("V0.NoSuchExample", client, new string[] { });
+                        }
+                    );
+                }, null
+            );
+
+            // Verify that a MissingMethodException is thrown if the specified example
+            // does not have a Run() method.
+            CaptureConsoleIOAndExecute(
+                delegate ()
+                {
+                    Assert.Throws(typeof(MissingMethodException),
+                        delegate ()
+                        {
+                            runner.Run(NoRunTestExample.TEST_EXAMPLE_NAME, client,
+                                new string[] { });
+                        }
+                    );
+                }, null
+            );
+
+            // Verify that a ArgumentException is thrown if an unknown flag is passed.
+            CaptureConsoleIOAndExecute(
+                delegate ()
+                {
+                    Assert.Throws(typeof(ArgumentException),
+                        delegate ()
+                        {
+                            runner.Run(TestExample.TEST_EXAMPLE_NAME, client,
+                                new string[] { "--foo=bar" });
+                        }
+                    );
+                }, null
+            );
+        }
+
+        /// <summary>
+        /// Captures the console input + output and execute a method.
+        /// </summary>
+        /// <param name="functionToRun">The function to run. This function should not read any
+        /// input from the console.</param>
+        /// <param name="afterRunCallback">The callback to be invoked after
+        /// <paramref name="functionToRun"/> completes. The console output is passed as an
+        /// argument to the action.</param>
+        private void CaptureConsoleIOAndExecute(TestDelegate functionToRun,
+            Action<string> afterRunCallback)
+        {
+            StringWriter writer = new StringWriter();
+            TextWriter oldWriter = null;
+            TextReader oldReader = null;
+            StringReader reader = new StringReader("\r\n");
+
+            try
+            {
+                // Capture the console input and output.
+                oldWriter = Console.Out;
+                Console.SetOut(writer);
+                oldReader = Console.In;
+                Console.SetIn(reader);
+
+                functionToRun?.Invoke();
+                afterRunCallback?.Invoke(writer.ToString());
+            }
+            finally
+            {
+                // Restore the console input and output.
+                Console.SetIn(oldReader);
+                Console.SetOut(oldWriter);
+            }
+        }
+    }
+}
