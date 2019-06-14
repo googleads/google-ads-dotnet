1df950aba31978f2b299d11170a1ee5cb1959683
Anash P. Oommen <AnashOommen@users.noreply.github.com>
2019-06-14 13:31:48 -0400

    Add examples for Campaign Draft and experiment creation (#105)

---
+++ examples/CampaignManagement/AddCampaignDraft.cs
@@ -0,0 +1,189 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.V1.Errors;
+using Google.Ads.GoogleAds.V1.Common;
+using Google.Ads.GoogleAds.V1.Resources;
+using Google.Ads.GoogleAds.V1.Services;
+
+using System;
+using System.Linq;
+
+namespace Google.Ads.GoogleAds.Examples.V1
+{
+    /// <summary>
+    /// This code example adds a campaign draft for a campaign. Make sure you specify a
+    /// campaign that has a budget with explicitly_shared set to false.
+    /// </summary>
+    public class AddCampaignDraft : ExampleBase
+    {
+        /// <summary>
+        /// Main method, to run this code example as a standalone application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            AddCampaignDraft codeExample = new AddCampaignDraft();
+            Console.WriteLine(codeExample.Description);
+
+            // The Google Ads customer ID for which the call is made.
+            long customerId = long.Parse("INSERT_CUSTOMER_ID_HERE");
+
+            // ID of the base campaign.
+            long baseCampaignId = long.Parse("INSERT_BASE_CAMPAIGN_ID_HERE");
+
+            codeExample.Run(new GoogleAdsClient(), customerId, baseCampaignId);
+        }
+
+        /// <summary>
+        /// Returns a description about the code example.
+        /// </summary>
+        public override string Description
+        {
+            get
+            {
+                return "This code example adds a campaign draft for a campaign. Make sure you " +
+                    "specify a campaign that has a budget with explicitly_shared set to false.";
+            }
+        }
+
+        /// <summary>
+        /// Runs the code example.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for which the call is made.</param>
+        /// <param name="baseCampaignId">ID of the base campaign</param>
+        public void Run(GoogleAdsClient client, long customerId, long baseCampaignId)
+        {
+            try
+            {
+                string draftResourceName = CreateCampaignDraft(client, customerId, baseCampaignId);
+                string draftCampaignResourceName = FetchDraftCampaign(client, customerId,
+                    draftResourceName);
+                AddLanguageCriteria(client, customerId, draftCampaignResourceName);
+            }
+            catch (GoogleAdsException e)
+            {
+                Console.WriteLine("Failure:");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Failure: {e.Failure}");
+                Console.WriteLine($"Request ID: {e.RequestId}");
+            }
+        }
+
+        /// <summary>
+        /// Creates the campaign draft.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for which the call is made.</param>
+        /// <param name="baseCampaignId">The base campaign ID.</param>
+        /// <returns></returns>
+        private static string CreateCampaignDraft(GoogleAdsClient client, long customerId,
+            long baseCampaignId)
+        {
+            // Get the CampaignDraftService.
+            CampaignDraftServiceClient campaignDraftService =
+                client.GetService(Services.V1.CampaignDraftService);
+
+            CampaignDraft campaignDraft = new CampaignDraft()
+            {
+                BaseCampaign = ResourceNames.Campaign(customerId, baseCampaignId),
+                Name = "Campaign Draft #" + ExampleUtilities.GetRandomString(),
+            };
+
+            CampaignDraftOperation operation = new CampaignDraftOperation()
+            {
+                Create = campaignDraft
+            };
+
+            MutateCampaignDraftsResponse response = campaignDraftService.MutateCampaignDrafts(
+                customerId.ToString(), new CampaignDraftOperation[] { operation });
+
+            string draftResourceName = response.Results[0].ResourceName;
+            Console.WriteLine($"Campaign with resource ID = '{draftResourceName}' was added.");
+            return draftResourceName;
+        }
+
+        /// <summary>
+        /// Fetches the draft campaign associated with a campaign draft.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for which the call is made.</param>
+        /// <param name="draftResourceName">Resource name of the draft.</param>
+        /// <returns></returns>
+        private static string FetchDraftCampaign(GoogleAdsClient client, long customerId,
+            string draftResourceName)
+        {
+            // Get the GoogleAdsService.
+            GoogleAdsServiceClient googleAdsService =
+                client.GetService(Services.V1.GoogleAdsService);
+
+            // Once the draft is created, you can modify the draft campaign as if it were
+            // a real campaign. For example, you may add criteria, adjust bids, or even
+            // include additional ads. Adding a criterion is shown here.
+            string query = $@"
+                SELECT
+                    campaign_draft.draft_campaign
+                FROM
+                    campaign_draft
+                WHERE
+                    campaign_draft.resource_name = '{draftResourceName}'";
+
+
+            // Get the draft campaign resource name.
+            string draftCampaignResourceName = googleAdsService.Search(
+                customerId.ToString(), query).First().CampaignDraft.DraftCampaign;
+            return draftCampaignResourceName;
+        }
+
+        /// <summary>
+        /// Adds a language criterion to the draft campaign.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for which the call is made.</param>
+        /// /// <param name="draftCampaignResourceName">Resource name of the draft campaign.</param>
+        private static void AddLanguageCriteria(GoogleAdsClient client, long customerId,
+            string draftCampaignResourceName)
+        {
+            // Get the CampaignCriterionService.
+            CampaignCriterionServiceClient campaignCriterionService =
+                client.GetService(Services.V1.CampaignCriterionService);
+
+            // Add a language criterion.
+            CampaignCriterionOperation criterionOperation = new CampaignCriterionOperation()
+            {
+                Create = new CampaignCriterion()
+                {
+                    Language = new LanguageInfo()
+                    {
+                        // Spanish
+                        LanguageConstant = ResourceNames.LanguageConstant(1003)
+                    },
+                    Campaign = draftCampaignResourceName
+                }
+            };
+
+            MutateCampaignCriteriaResponse campaignCriteriaResponse =
+                campaignCriterionService.MutateCampaignCriteria(
+                    customerId.ToString(),
+                    new CampaignCriterionOperation[] { criterionOperation });
+
+            string newCampaignCriterionResource = campaignCriteriaResponse.Results[0].ResourceName;
+            Console.WriteLine($"Campaign Criterion with resource ID = " +
+                $"'{newCampaignCriterionResource}' was added to campaign with resource ID = " +
+                $"'{draftCampaignResourceName}'.");
+        }
+    }
+}
