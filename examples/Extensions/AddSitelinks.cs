5287f2dfc73e3ef2b500cdcc97e8520ba203e674
AnashOommen <adwordsapiadvisor+anash@google.com>
2020-01-06 12:11:22 -0500

    Second batch of new code examples

---
+++ examples/Extensions/AddSitelinks.cs
@@ -0,0 +1,283 @@
+﻿// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.V2.Common;
+using Google.Ads.GoogleAds.V2.Enums;
+using Google.Ads.GoogleAds.V2.Errors;
+using Google.Ads.GoogleAds.V2.Resources;
+using Google.Ads.GoogleAds.V2.Services;
+
+using System;
+using System.Collections.Generic;
+
+using static Google.Ads.GoogleAds.V2.Enums.ExtensionTypeEnum.Types;
+using static Google.Ads.GoogleAds.V2.Enums.FeedItemTargetDeviceEnum.Types;
+using static Google.Ads.GoogleAds.V2.Enums.KeywordMatchTypeEnum.Types;
+using static Google.Ads.GoogleAds.V2.Enums.MinuteOfHourEnum.Types;
+
+namespace Google.Ads.GoogleAds.Examples.V2
+{
+    /// <summary>
+    /// This code example adds sitelinks to a campaign. To create a campaign, run AddCampaigns.cs.
+    /// </summary>
+    public class AddSitelinks : ExampleBase
+    {
+        /// <summary>
+        /// Main method, to run this code example as a standalone application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            AddSitelinks codeExample = new AddSitelinks();
+            Console.WriteLine(codeExample.Description);
+
+            // The customer ID for which the call is made.
+            int customerId = int.Parse("INSERT_CUSTOMER_ID_HERE");
+
+            // ID of the campaign to which sitelinks are added.
+            long campaignId = long.Parse("INSERT_CAMPAIGN_ID_HERE");
+
+            codeExample.Run(new GoogleAdsClient(), customerId, campaignId);
+        }
+
+        /// <summary>
+        /// Returns a description about the code example.
+        /// </summary>
+        public override string Description
+        {
+            get
+            {
+                return "This code example adds sitelinks to a campaign. To create a campaign, " +
+                    "run AddCampaigns.cs.";
+            }
+        }
+
+        /// <summary>
+        /// Runs the code example.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The customer ID for which the call is made.</param>
+        /// <param name="campaignId">ID of the campaign to which sitelinks are added.</param>
+        public void Run(GoogleAdsClient client, long customerId, long campaignId)
+        {
+            // Get the CampaignExtensionSettingServiceClient.
+            CampaignExtensionSettingServiceClient campaignExtensionSettingService =
+                client.GetService(Services.V2.CampaignExtensionSettingService);
+
+            string campaignResourceName = ResourceNames.Campaign(customerId, campaignId);
+
+            List<string> extensionFeedItems =
+                CreateExtensionFeedItems(client, customerId, campaignResourceName);
+
+            // Creates a CampaignExtensionSetting.
+            CampaignExtensionSetting campaignExtensionSetting = new CampaignExtensionSetting()
+            {
+                Campaign = campaignResourceName,
+                ExtensionType = ExtensionType.Sitelink,
+                ExtensionFeedItems = { extensionFeedItems }
+            };
+
+            // Creates the operation.
+            CampaignExtensionSettingOperation operation = new CampaignExtensionSettingOperation()
+            {
+                Create = campaignExtensionSetting
+            };
+
+            try
+            {
+                MutateCampaignExtensionSettingsResponse response =
+                    campaignExtensionSettingService.MutateCampaignExtensionSettings(
+                        customerId.ToString(), new[] { operation });
+                foreach (MutateCampaignExtensionSettingResult result in response.Results)
+                {
+                    Console.WriteLine($"Created CampaignExtensionSetting with resource" +
+                        $" name '{result.ResourceName}'.");
+                }
+            }
+            catch (GoogleAdsException e)
+            {
+                Console.WriteLine("Failure:");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Failure: {e.Failure}");
+                Console.WriteLine($"Request ID: {e.RequestId}");
+            }
+        }
+
+        /// <summary>
+        /// Creates a list of ExtensionFeedItems.
+        /// </summary>
+        ///<param name="client">The Google Ads API client.</param>
+        ///<param name="customerId">The client customer ID.</param>
+        ///<param name="campaignResourceName">The resource name of the campaign to target.</param>
+        private static List<string> CreateExtensionFeedItems(GoogleAdsClient client,
+            long customerId, string campaignResourceName)
+        {
+            // Get the ExtensionFeedItemServiceClient.
+            ExtensionFeedItemServiceClient extensionFeedItemService =
+                client.GetService(Services.V2.ExtensionFeedItemService);
+
+            SitelinkFeedItem sitelinkFeedItem1 = CreateSitelinkFeedItem(
+                "Store Hours", "http://www.example.com/storehours");
+
+            // Creates an ExtensionFeedItem from the SitelinkFeedItem.
+            ExtensionFeedItem extensionFeedItem1 = new ExtensionFeedItem()
+            {
+                ExtensionType = ExtensionType.Sitelink,
+                SitelinkFeedItem = sitelinkFeedItem1,
+                TargetedCampaign = campaignResourceName
+            };
+
+            List<ExtensionFeedItemOperation> operations = new List<ExtensionFeedItemOperation>();
+
+            // Creates an ExtensionFeedItemOperation and adds it to the operations List.
+            operations.Add(new ExtensionFeedItemOperation() { Create = extensionFeedItem1 });
+
+            SitelinkFeedItem sitelinkFeedItem2 = CreateSitelinkFeedItem(
+                "Thanksgiving Specials", "http://www.example.com/thanksgiving");
+
+            DateTime startTime = new DateTime(DateTime.Now.Year, 11, 20, 0, 0, 0);
+            if (startTime < DateTime.Now)
+            {
+                // Move the startTime to next year if the current date is past November 20th.
+                startTime = startTime.AddYears(1);
+            }
+
+            // Converts to a string in the required format.
+            string startTimeString = startTime.ToString("yyyy-MM-dd hh:mm:ss");
+
+            // Use the same year as startTime when creating endTime.
+            DateTime endTime = new DateTime(startTime.Year, 11, 27, 23, 59, 59);
+
+            string unitedStates = ResourceNames.GeoTargetConstant(2840);
+
+            ExtensionFeedItem extensionFeedItem2 = new ExtensionFeedItem()
+            {
+                ExtensionType = ExtensionType.Sitelink,
+                SitelinkFeedItem = sitelinkFeedItem2,
+                TargetedCampaign = campaignResourceName,
+
+                // StartDateTime should be formatted in "yyyy-MM-dd hh:mm:ss" format.
+                StartDateTime = startTime.ToString("yyyy-MM-dd hh:mm:ss"),
+
+                // EndDateTime should be formatted in "yyyy-MM-dd hh:mm:ss" format.
+                EndDateTime = endTime.ToString("yyyy-MM-dd hh:mm:ss"),
+
+                // Targets this sitelink for United States only.
+                // A list of country codes can be referenced here:
+                // https://developers.google.com/adwords/api/docs/appendix/geotargeting
+                TargetedGeoTargetConstant = ResourceNames.GeoTargetConstant(2840)
+            };
+
+            operations.Add(new ExtensionFeedItemOperation() { Create = extensionFeedItem2 });
+
+            SitelinkFeedItem sitelinkFeedItem3 = CreateSitelinkFeedItem(
+                "Wifi available", "http://www.example.com/mobile/wifi");
+
+            ExtensionFeedItem extensionFeedItem3 = new ExtensionFeedItem()
+            {
+                ExtensionType = ExtensionType.Sitelink,
+                SitelinkFeedItem = sitelinkFeedItem3,
+                TargetedCampaign = campaignResourceName,
+                Device = FeedItemTargetDevice.Mobile,
+                TargetedKeyword = new KeywordInfo()
+                {
+                    Text = "free wifi",
+                    MatchType = KeywordMatchType.Broad
+                }
+            };
+
+            operations.Add(new ExtensionFeedItemOperation() { Create = extensionFeedItem3 });
+
+            SitelinkFeedItem sitelinkFeedItem4 = CreateSitelinkFeedItem(
+                "Happy hours", "http://www.example.com/happyhours");
+
+            ExtensionFeedItem extensionFeedItem4 = new ExtensionFeedItem()
+            {
+                ExtensionType = ExtensionType.Sitelink,
+                SitelinkFeedItem = sitelinkFeedItem4,
+                TargetedCampaign = campaignResourceName,
+                AdSchedules = {
+                    CreateAdScheduleInfo(DayOfWeekEnum.Types.DayOfWeek.Monday, 18,
+                        MinuteOfHour.Zero, 21, MinuteOfHour.Zero),
+                    CreateAdScheduleInfo(DayOfWeekEnum.Types.DayOfWeek.Tuesday, 18,
+                        MinuteOfHour.Zero, 21, MinuteOfHour.Zero),
+                    CreateAdScheduleInfo(DayOfWeekEnum.Types.DayOfWeek.Wednesday, 18,
+                        MinuteOfHour.Zero, 21, MinuteOfHour.Zero),
+                    CreateAdScheduleInfo(DayOfWeekEnum.Types.DayOfWeek.Thursday, 18,
+                        MinuteOfHour.Zero, 21, MinuteOfHour.Zero),
+                    CreateAdScheduleInfo(DayOfWeekEnum.Types.DayOfWeek.Friday, 18,
+                        MinuteOfHour.Zero, 21, MinuteOfHour.Zero),
+                }
+            };
+
+            operations.Add(new ExtensionFeedItemOperation() { Create = extensionFeedItem4 });
+
+            // Adds the ExtensionFeedItem.
+            MutateExtensionFeedItemsResponse response =
+                extensionFeedItemService.MutateExtensionFeedItems(customerId.ToString(),
+                    operations);
+            Console.WriteLine($"Added {response.Results.Count}:");
+
+            List<string> resourceNames = new List<string>();
+            foreach (MutateExtensionFeedItemResult result in response.Results)
+            {
+                Console.WriteLine($"Created ExtensionFeedItems with " +
+                    $"resource name '{result.ResourceName}'.");
+                resourceNames.Add(result.ResourceName);
+            }
+            return resourceNames;
+        }
+
+
+        /// <summary>
+        /// Creates a new SitelinkFeedItem with the specified attributes.
+        /// </summary>
+        /// <param name="sitelinkText">The text of the sitelink feed item.</param>
+        /// <param name="sitelinkUrl">The URL of the sitelink feed item.</param>
+        /// <returns>The sitelink feed item.</returns>
+        private static SitelinkFeedItem CreateSitelinkFeedItem(string sitelinkText,
+        string sitelinkUrl)
+        {
+            return new SitelinkFeedItem()
+            {
+                LinkText = sitelinkText,
+                FinalUrls = { sitelinkUrl }
+            };
+        }
+
+        /// <summary>
+        /// Creates a new AdScheduleInfo with the specified attributes.
+        /// </summary>
+        /// <param name="day">Day of the week of the AdScheduleInfo.</param>
+        /// <param name="startHour">The starting hour of the AdScheduleInfo.</param>
+        /// <param name="startMinute">The starting minute of the AdScheduleInfo.</param>
+        /// <param name="endHour">The ending hour of the AdScheduleInfo.</param>
+        /// <param name="endMinute">The ending minute of the AdScheduleInfo.</param>
+        /// <returns>The ad schedule info.</returns>
+        private static AdScheduleInfo CreateAdScheduleInfo(DayOfWeekEnum.Types.DayOfWeek day,
+                    int startHour, MinuteOfHour startMinute, int endHour, MinuteOfHour endMinute)
+        {
+            return new AdScheduleInfo()
+            {
+                DayOfWeek = day,
+                StartHour = startHour,
+                StartMinute = startMinute,
+                EndHour = endHour,
+                EndMinute = endMinute
+            };
+        }
+    }
+}
---
+++ examples/Extensions/UpdateFeedItemAttributeValue.cs
@@ -0,0 +1,314 @@
+﻿// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.Util;
+using Google.Ads.GoogleAds.V2.Errors;
+using Google.Ads.GoogleAds.V2.Resources;
+using Google.Ads.GoogleAds.V2.Services;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Google.Ads.GoogleAds.V2.Enums.FlightPlaceholderFieldEnum.Types;
+
+namespace Google.Ads.GoogleAds.Examples.V2
+{
+    /// <summary>
+    /// This code example updates a FeedItemAttributeValue in a flights feed. To create a flights
+    /// feed, run the AddFlightsFeed.cs. This code example is specific to feeds of type
+    /// DYNAMIC_FLIGHT.The attribute you are updating must be present on the feed. This code
+    /// example is specifically for updating the StringValue of an attribute.
+    /// </summary>
+    public class UpdateFeedItemAttributeValue : ExampleBase
+    {
+        /// <summary>
+        /// Main method, to run this code example as a standalone application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            UpdateFeedItemAttributeValue codeExample = new UpdateFeedItemAttributeValue();
+            Console.WriteLine(codeExample.Description);
+
+            // The customer ID for which the call is made.
+            int customerId = int.Parse("INSERT_CUSTOMER_ID_HERE");
+
+            // ID of the feed containing the feed item to be updated.
+            long feedId = long.Parse("INSERT_FEED_ID_HERE");
+
+            // ID of the feed item to be updated.
+            long feedItemId = long.Parse("INSERT_FEED_ITEM_ID_HERE");
+
+            // The placeholder type for the attribute to be removed.
+            string flightPlaceholderField = "INSERT_FLIGHT_PLACEHOLDER_FIELD_HERE";
+            
+            // Value with which to update the FeedAttributeValue.
+            string attributeValue = "INSERT_ATTRIBUTE_VALUE_HERE";
+
+            codeExample.Run(new GoogleAdsClient(), customerId, feedId, feedItemId,
+                flightPlaceholderField, attributeValue);
+        }
+
+        /// <summary>
+        /// Returns a description about the code example.
+        /// </summary>
+        public override string Description
+        {
+            get
+            {
+                return "This code example updates a FeedItemAttributeValue in a flights feed. " +
+                    "To create a flights feed, run the AddFlightsFeed.cs. This code example is " +
+                    "specific to feeds of type DYNAMIC_FLIGHT. The attribute you are updating " +
+                    "must be present on the feed. This code example is specifically for updating " +
+                    "the StringValue of an attribute.";
+            }
+        }
+
+        /// <summary>
+        /// Runs the code example.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The customer ID for which the call is made.</param>
+        /// <param name="feedId">ID of the feed containing the feed item to be updated.</param>
+        /// <param name="feedItemId">ID of the feed item to be updated.</param>
+        /// <param name="flightPlaceholderField">The placeholder type for the attribute to be
+        /// removed.</param>
+        /// <param name="attributeValue">Value with which to update the FeedAttributeValue.</param>
+        public void Run(GoogleAdsClient client, long customerId, long feedId, long feedItemId,
+            string flightPlaceholderField, string attributeValue)
+        {
+            try
+            {
+                UpdateFeedItem(client, customerId, feedId, feedItemId, flightPlaceholderField,
+                    attributeValue);
+            }
+            catch (GoogleAdsException e)
+            {
+                Console.WriteLine("Failure:");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Failure: {e.Failure}");
+                Console.WriteLine($"Request ID: {e.RequestId}");
+            }
+        }
+
+        /// <summary>
+        /// Updates attribute value of the feed item. In order to update a FeedItemAttributeValue
+        /// you must update the FeedItem.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The customer ID for which the call is made.</param>
+        /// <param name="feedId">ID of the feed containing the feed item to be updated.</param>
+        /// <param name="feedItemId">ID of the feed item to be updated.</param>
+        /// <param name="flightPlaceholderField">the placeholder type for the attribute to be
+        /// updated.</param>
+        /// <param name="attributeValue">String value with which to update the
+        /// FeedAttributeValue.</param>
+        private void UpdateFeedItem(GoogleAdsClient client, long customerId, long feedId,
+                    long feedItemId, string flightPlaceholderField, string attributeValue)
+        {
+            // Get the FeedItemServiceClient.
+            FeedItemServiceClient feedItemService =
+                client.GetService(Services.V2.FeedItemService);
+
+            // Gets the feed resource name.
+            string feedResourceName = ResourceNames.Feed(customerId, feedId);
+
+            // Gets a map of the placeholder values and feed attributes.
+            Dictionary<FlightPlaceholderField, FeedAttribute> feedAttributes =
+                GetFeed(client, customerId, feedResourceName);
+
+            // Gets the ID of the attribute to update. This is needed to specify which
+            // FeedItemAttributeValue will be updated in the given FeedItem.
+
+            FlightPlaceholderField placeholderField = (FlightPlaceholderField) Enum.Parse(
+                typeof(FlightPlaceholderField), flightPlaceholderField);
+            long attributeId = feedAttributes[placeholderField].Id.Value;
+
+            // Gets the feed item resource name.
+            string feedItemResourceName = ResourceNames.FeedItem(customerId, feedId, feedItemId);
+            // Retrieves the feed item and its associated attributes based on its resource name.
+            FeedItem feedItem = GetFeedItem(client, customerId, feedItemResourceName);
+            // Creates the updated FeedItemAttributeValue.
+            FeedItemAttributeValue feedItemAttributeValue = new FeedItemAttributeValue()
+            {
+                FeedAttributeId = attributeId,
+                StringValue = attributeValue
+            };
+
+            // Creates a new FeedItem from the existing FeedItem. Any FeedItemAttributeValues that are
+            // not included in the updated FeedItem will be removed from the FeedItem, which is why you
+            // must create the FeedItem from the existing FeedItem and set the field(s) that will be
+            // updated.
+            feedItem.AttributeValues[GetAttributeIndex(feedItem, feedItemAttributeValue)] =
+                feedItemAttributeValue;
+
+            // Creates the operation.
+            FeedItemOperation operation = new FeedItemOperation()
+            {
+                Update = feedItem,
+                UpdateMask = FieldMasks.AllSetFieldsOf(feedItem)
+            };
+
+            // Updates the feed item.
+            MutateFeedItemsResponse response =
+                feedItemService.MutateFeedItems(customerId.ToString(), new[] { operation });
+
+            foreach (MutateFeedItemResult result in response.Results)
+            {
+                Console.WriteLine($"Updated feed item with resource name '{result.ResourceName}'.");
+            }
+        }
+
+        /// <summary>
+        /// Retrieves details about a feed. The initial query retrieves the FeedAttributes,
+        /// or columns, of the feed. Each FeedAttribute will also include the FeedAttributeId,
+        /// which will be used in a subsequent step. The example then inserts a new key, value
+        /// pair into a map for each FeedAttribute, which is the return value of the method.
+        /// The keys are the placeholder types that the columns will be. The values are the
+        /// FeedAttributes.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for which the flights feed is
+        /// added.</param>
+        /// <param name="feedResourceName">The resource name of the feed.</param>
+        /// <returns>
+        /// A Map containing the FlightPlaceholderField and FeedAttribute.
+        /// </returns>
+        public Dictionary<FlightPlaceholderField, FeedAttribute> GetFeed(
+            GoogleAdsClient client, long customerId, string feedResourceName)
+        {
+            // Get the GoogleAdsService.
+            GoogleAdsServiceClient googleAdsService = client.GetService(
+                Services.V2.GoogleAdsService);
+
+            // Constructs the query.
+            string query = $"SELECT feed.attributes FROM feed WHERE feed.resource_name = " +
+                $"'{feedResourceName}'";
+
+            // Constructs the request.
+            SearchGoogleAdsRequest request = new SearchGoogleAdsRequest()
+            {
+                CustomerId = customerId.ToString(),
+                Query = query
+            };
+
+            // Issues the search request and get the first result, since we only need the
+            // single feed item we created previously..
+            GoogleAdsRow googleAdsRow = googleAdsService.Search(request).First();
+
+            // Gets the attributes list from the feed and creates a map with keys of each attribute and
+            // values of each corresponding ID.
+
+            Dictionary<FlightPlaceholderField, FeedAttribute> feedAttributes =
+                new Dictionary<FlightPlaceholderField, FeedAttribute>();
+
+            // Loops through the feed attributes to populate the map.
+            foreach (FeedAttribute feedAttribute in googleAdsRow.Feed.Attributes)
+            {
+                switch (feedAttribute.Name)
+                {
+                    case "Flight Description":
+                        feedAttributes[FlightPlaceholderField.FlightDescription] = feedAttribute;
+                        break;
+
+                    case "Destination ID":
+                        feedAttributes[FlightPlaceholderField.DestinationId] = feedAttribute;
+                        break;
+
+                    case "Flight Price":
+                        feedAttributes[FlightPlaceholderField.FlightPrice] = feedAttribute;
+                        break;
+
+                    case "Flight Sale Price":
+                        feedAttributes[FlightPlaceholderField.FlightSalePrice] = feedAttribute;
+                        break;
+
+                    case "Final URLs":
+                        feedAttributes[FlightPlaceholderField.FinalUrls] = feedAttribute;
+                        break;
+                    // The full list of FlightPlaceholderFields can be found here
+                    // https://developers.google.com/google-ads/api/reference/rpc/google.ads.googleads.v2.enums#flightplaceholderfieldenum.
+                    default:
+                        throw new Exception("Invalid attribute name.");
+                }
+            }
+            return feedAttributes;
+        }
+
+        /// <summary>
+        /// Retrieves a feed item and its attribute values given a resource name.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for which the flights feed is
+        /// added.</param>
+        /// <param name="feedItemResourceName">Feed item resource name.</param>
+        /// <returns>FeedItem with the given resource name.</returns>
+        private FeedItem GetFeedItem(GoogleAdsClient client, long customerId,
+            string feedItemResourceName)
+        {
+            // Get the GoogleAdsService.
+            GoogleAdsServiceClient googleAdsService = client.GetService(
+                Services.V2.GoogleAdsService);
+
+            // Constructs the query.
+            string query = "SELECT feed_item.attribute_values FROM feed_item WHERE " +
+                $"feed_item.resource_name = '{feedItemResourceName}'";
+
+            // Constructs the request.
+            SearchGoogleAdsRequest request = new SearchGoogleAdsRequest()
+            {
+                CustomerId = customerId.ToString(),
+                Query = query
+            };
+
+            return googleAdsService.Search(request).First().FeedItem;
+        }
+
+        /// <summary>
+        /// Gets the ID of the attribute. This is needed to specify which FeedItemAttributeValue
+        /// will be updated in the given FeedItem.
+        /// </summary>
+        /// <param name="feedItem">The FeedItem that will be updated.</param>
+        /// <param name="newFeedItemAttributeValue">The new FeedItemAttributeValue that will be
+        /// updated.</param>
+        /// <returns>The index of the attribute.</returns>
+        private int GetAttributeIndex(FeedItem feedItem,
+            FeedItemAttributeValue newFeedItemAttributeValue)
+        {
+            int attributeIndex = -1;
+            // Loops through attribute values to find the index of the FeedItemAttributeValue
+            // to update.
+            foreach (FeedItemAttributeValue feedItemAttributeValue in feedItem.AttributeValues)
+            {
+                attributeIndex = (attributeIndex == -1) ? attributeIndex + 1 : 0;
+                // Checks if the current feedItemAttributeValue is the one we are updating
+                if (feedItemAttributeValue.FeedAttributeId
+                  == newFeedItemAttributeValue.FeedAttributeId)
+                {
+                    break;
+                }
+            }
+
+            // Throws an exception if the attribute value is not found.
+            if (attributeIndex == -1)
+            {
+                new ArgumentException($"No matching feed attribute for feed item attribute " +
+                    $"value: {newFeedItemAttributeValue}");
+            }
+
+            return attributeIndex;
+        }
+    }
+}
