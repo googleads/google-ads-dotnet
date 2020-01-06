368da08380049008d30deeca5a61f8935bae6d8a
AnashOommen <adwordsapiadvisor+anash@google.com>
2020-01-06 12:31:55 -0500

    Fix a few broken tests due to refactoring

--- tests/StubIntegrityTestHelper.cs
+++ tests/StubIntegrityTestHelper.cs
@@ -18,96 +18,104 @@ using System.Reflection;
 
 using SystemType = System.Type;
 
-namespace Google.Ads.GoogleAds.Tests {
-
-  /// <summary>
-  /// Utility class that provides functionality to test the integrity of
-  /// generated stub code for various API services.
-  /// </summary>
-  public class StubIntegrityTestHelper {
-
+namespace Google.Ads.GoogleAds.Tests
+{
     /// <summary>
-    /// The callback to be called when the <see cref="EnumerateServices"/> method
-    /// finds a matching service signature.
+    /// Utility class that provides functionality to test the integrity of
+    /// generated stub code for various API services.
     /// </summary>
-    /// <param name="signature">The signature.</param>
-    public delegate void EnumerateServiceCallback(SystemType signatureType);
+    public class StubIntegrityTestHelper
+    {
+        /// <summary>
+        /// The callback to be called when the <see cref="EnumerateServices"/> method
+        /// finds a matching service signature.
+        /// </summary>
+        /// <param name="serviceSignature">The service signature.</param>
+        public delegate void EnumerateServiceCallback(object serviceSignature);
 
-    /// <summary>
-    /// The callback to be called when the <see cref="EnumerateEnumFields"/> method
-    /// finds a matching service signature.
-    /// </summary>
-    /// <param name="hashedFieldName">The field name in a hashed format
-    /// (typename_fieldname).</param>
-    /// <param name="enumValue">The enum value.</param>
-    public delegate void EnumerateEnumFieldsCallback(string hashedFieldName, int enumValue);
+        /// <summary>
+        /// The callback to be called when the <see cref="EnumerateEnumFields"/> method
+        /// finds a matching service signature.
+        /// </summary>
+        /// <param name="hashedFieldName">The field name in a hashed format
+        /// (typename_fieldname).</param>
+        /// <param name="enumValue">The enum value.</param>
+        public delegate void EnumerateEnumFieldsCallback(string hashedFieldName, int enumValue);
 
-    /// <summary>
-    /// Enumerates the services supported in a Ads Service.
-    /// </summary>
-    /// <typeparam name="ServiceType">The Ads service being tested
-    /// (e.g. <see cref="Services"/>)</typeparam>
-    /// <param name="onServiceFound">The callback to be called when a matching
-    /// service is found.</param>
-    public static void EnumerateServices<ServiceType>(EnumerateServiceCallback onServiceFound) {
-      SystemType serviceType = typeof(ServiceType);
+        /// <summary>
+        /// Enumerates the services supported in a Ads Service.
+        /// </summary>
+        /// <typeparam name="ServiceType">The Ads service being tested
+        /// (e.g. <see cref="Services"/>)</typeparam>
+        /// <param name="onServiceFound">The callback to be called when a matching
+        /// service is found.</param>
+        public static void EnumerateServices<ServiceType>(EnumerateServiceCallback onServiceFound)
+        {
+            SystemType serviceType = typeof(ServiceType);
 
-      // All the supported service types are defined as nested classes within the
-      // service type. Each of the nested type has multiple ServiceTemplate entries,
-      // one per supported service. This allows user to create a service like:
-      //
-      // var campaignService = user.GetService(Services.V0.ReportingService);
-      SystemType[] versionTypes = serviceType.GetNestedTypes();
+            // All the supported service types are defined as nested classes within the
+            // service type. Each of the nested type has multiple ServiceTemplate entries,
+            // one per supported service. This allows user to create a service like:
+            //
+            // var campaignService = user.GetService(Services.V0.ReportingService);
+            SystemType[] versionTypes = serviceType.GetNestedTypes();
 
-      foreach (SystemType versionType in versionTypes) {
-        FieldInfo[] serviceFields = versionType.GetFields
-            (BindingFlags.Static | BindingFlags.Public);
-        foreach (FieldInfo fieldInfo in serviceFields) {
-          onServiceFound(fieldInfo.FieldType);
+            foreach (SystemType versionType in versionTypes)
+            {
+                FieldInfo[] serviceFields = versionType.GetFields
+                    (BindingFlags.Static | BindingFlags.Public);
+                foreach (FieldInfo fieldInfo in serviceFields)
+                {
+                    onServiceFound(fieldInfo.GetValue(null));
+                }
+            }
         }
-      }
-    }
 
-    /// <summary>
-    /// Enumerates the enum fields and values under a given root namespace.
-    /// </summary>
-    /// <typeparam name="ServiceType"></typeparam>
-    /// <param name="rootNameSpace">The root name space.</param>
-    /// <param name="onEnumFieldDeclarationFound">The callback to be called when
-    /// a matching enumeration field is found.</param>
-    public static void EnumerateEnumFields<ServiceType>(string rootNameSpace,
-        EnumerateEnumFieldsCallback onEnumFieldDeclarationFound) {
-      // Find all supported versions.
-      SystemType serviceType = typeof(ServiceType);
-      SystemType[] versionTypes = serviceType.GetNestedTypes();
+        /// <summary>
+        /// Enumerates the enum fields and values under a given root namespace.
+        /// </summary>
+        /// <typeparam name="ServiceType"></typeparam>
+        /// <param name="rootNameSpace">The root name space.</param>
+        /// <param name="onEnumFieldDeclarationFound">The callback to be called when
+        /// a matching enumeration field is found.</param>
+        public static void EnumerateEnumFields<ServiceType>(string rootNameSpace,
+            EnumerateEnumFieldsCallback onEnumFieldDeclarationFound)
+        {
+            // Find all supported versions.
+            SystemType serviceType = typeof(ServiceType);
+            SystemType[] versionTypes = serviceType.GetNestedTypes();
 
-      ISet<string> rootEnums = new HashSet<string>();
+            ISet<string> rootEnums = new HashSet<string>();
 
-      // Find out the namespace for each supported API version.
-      foreach (SystemType versionType in versionTypes) {
-        string typeName = versionType.Name;
-        string namespaceName = rootNameSpace + typeName;
-        rootEnums.Add(namespaceName);
-      }
+            // Find out the namespace for each supported API version.
+            foreach (SystemType versionType in versionTypes)
+            {
+                string typeName = versionType.Name;
+                string namespaceName = rootNameSpace + typeName;
+                rootEnums.Add(namespaceName);
+            }
 
-      // Enumerate all types in the client library.
-      foreach (SystemType declaredType in serviceType.Assembly.GetTypes()) {
-        // If this is an enumeration within a namespace that corresponds to a
-        // supported API version then process further.
-        if (rootEnums.Contains(declaredType.Namespace) && declaredType.IsEnum) {
-          Array values = declaredType.GetEnumValues();
+            // Enumerate all types in the client library.
+            foreach (SystemType declaredType in serviceType.Assembly.GetTypes())
+            {
+                // If this is an enumeration within a namespace that corresponds to a
+                // supported API version then process further.
+                if (rootEnums.Contains(declaredType.Namespace) && declaredType.IsEnum)
+                {
+                    Array values = declaredType.GetEnumValues();
 
-          foreach (object value in values) {
-            string enumName = declaredType.GetEnumName(value);
+                    foreach (object value in values)
+                    {
+                        string enumName = declaredType.GetEnumName(value);
 
-            // Generate a unique name as EnumTypeName.EnumFieldName.
-            string hashedName = declaredType.Name + "." + enumName;
-            int enumValue = (int) value;
+                        // Generate a unique name as EnumTypeName.EnumFieldName.
+                        string hashedName = declaredType.Name + "." + enumName;
+                        int enumValue = (int) value;
 
-            onEnumFieldDeclarationFound(hashedName, enumValue);
-          }
+                        onEnumFieldDeclarationFound(hashedName, enumValue);
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
