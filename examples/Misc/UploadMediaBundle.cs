14156446c4a08ee169f874d49e52f5e15ed4d48d
AnashOommen <adwordsapiadvisor+anash@google.com>
2020-01-16 13:02:21 -0500

    New examples, set 2

---
+++ examples/Misc/UploadMediaBundle.cs
@@ -0,0 +1,112 @@
+﻿// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.Util;
+using Google.Ads.GoogleAds.V2.Errors;
+using Google.Ads.GoogleAds.V2.Resources;
+using Google.Ads.GoogleAds.V2.Services;
+using Google.Protobuf;
+using System;
+using System.Linq;
+using static Google.Ads.GoogleAds.V2.Enums.MediaTypeEnum.Types;
+
+namespace Google.Ads.GoogleAds.Examples.V2
+{
+    /// <summary>
+    /// This code example uploads an HTML5 zip file as a media bundle.
+    /// </summary>
+    public class UploadMediaBundle : ExampleBase
+    {
+        /// <summary>
+        /// The asset bundle to upload.
+        /// </summary>
+        private const string BUNDLE_URL = "https://goo.gl/9Y7qI2";
+
+        /// <summary>
+        /// Main method, to run this code example as a standalone application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            UploadMediaBundle codeExample = new UploadMediaBundle();
+            Console.WriteLine(codeExample.Description);
+
+            // The customer ID for which the call is made.
+            int customerId = int.Parse("INSERT_CUSTOMER_ID_HERE");
+
+            codeExample.Run(new GoogleAdsClient(), customerId);
+        }
+
+        /// <summary>
+        /// Returns a description about the code example.
+        /// </summary>
+        public override string Description
+        {
+            get
+            {
+                return "This code example uploads an HTML5 zip file as a media bundle.";
+            }
+        }
+
+        /// <summary>
+        /// Runs the code example.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The customer ID for which the call is made.</param>
+        public void Run(GoogleAdsClient client, long customerId)
+        {
+            // Get the MediaFileServiceClient.
+            MediaFileServiceClient mediaFileService = client.GetService(Services.V2.MediaFileService);
+
+            // Creates an HTML5 zip file media bundle content.
+            byte[] bundleContent = MediaUtilities.GetAssetDataFromUrl(BUNDLE_URL, client.Config);
+
+            // Creates a media file.
+            MediaFile mediaFile = new MediaFile()
+            {
+                Name = "Ad Media Bundle",
+                Type = MediaType.MediaBundle,
+                MediaBundle = new MediaBundle()
+                {
+                    Data = ByteString.CopyFrom(bundleContent),
+                }
+            };
+
+            // Creates a media file operation.
+            MediaFileOperation operation = new MediaFileOperation()
+            {
+                Create = mediaFile
+            };
+
+            try
+            {
+                // Issues a mutate request to add the media file.
+                MutateMediaFilesResponse response =
+                    mediaFileService.MutateMediaFiles(customerId.ToString(), new[] { operation });
+
+                // Displays the result.
+                Console.WriteLine($"The media bundle with resource name " +
+                    $"'{response.Results.First().ResourceName}' is created.");
+            }
+            catch (GoogleAdsException e)
+            {
+                Console.WriteLine("Failure:");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Failure: {e.Failure}");
+                Console.WriteLine($"Request ID: {e.RequestId}");
+            }
+        }
+    }
+}
---
+++ examples/Remarketing/UploadConversionAdjustment.cs
@@ -0,0 +1,145 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.V2.Errors;
+using Google.Ads.GoogleAds.V2.Services;
+
+using System;
+using static Google.Ads.GoogleAds.V2.Enums.ConversionAdjustmentTypeEnum.Types;
+
+namespace Google.Ads.GoogleAds.Examples.V2
+{
+    /// <summary>
+    /// This code example imports conversion adjustments for conversions that already exist.
+    /// To set up a conversion action, run AddConversionAction.cs.
+    /// </summary>
+    public class UploadConversionAdjustment : ExampleBase
+    {
+        /// <summary>
+        /// Main method, to run this code example as a standalone application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            UploadConversionAdjustment codeExample = new UploadConversionAdjustment();
+            Console.WriteLine(codeExample.Description);
+
+            // The Google Ads customer ID for which the call is made.
+            long customerId = long.Parse("INSERT_CUSTOMER_ID_HERE");
+
+            // ID of the conversion action for which adjustments are uploaded.
+            long conversionActionId = long.Parse("INSERT_CUSTOMER_ID_HERE");
+
+            // The type of adjustment.
+            ConversionAdjustmentType adjustmentType =
+                (ConversionAdjustmentType) Enum.Parse(
+                    typeof(ConversionAdjustmentType), "INSERT_ADJUSTMENT_TYPE_HERE");
+
+            // The original conversion time.
+            string conversionDateTime = "INSERT_CONVERSION_DATE_TIME_HERE";
+
+            // The Google Click ID for which adjustments are uploaded.
+            string gclid = "INSERT_GCLID_HERE";
+
+            // The adjustment date and time.
+            string adjustmentDateTime = "INSERT_ADJUSTMENT_DATE_TIME_HERE";
+
+            // The restatement value.
+            double? restatementValue = double.Parse("INSERT_RESTATEMENT_VALUE_HERE");
+
+            codeExample.Run(new GoogleAdsClient(), customerId, conversionActionId, gclid,
+                conversionDateTime, adjustmentDateTime, adjustmentType, restatementValue);
+        }
+
+        /// <summary>
+        /// Returns a description about the code example.
+        /// </summary>
+        public override string Description
+        {
+            get
+            {
+                return "This code example imports conversion adjustments for conversions that " +
+                    "already exist. To set up a conversion action, run AddConversionAction.cs.";
+            }
+        }
+
+        /// <summary>
+        /// Runs the code example.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for the conversion action is
+        /// added.</param>
+        /// <param name="conversionActionId">ID of the conversion action for which adjustments are
+        /// uploaded.</param>
+        /// <param name="adjustmentType">The type of adjustment.</param>
+        /// <param name="conversionDateTime">The original conversion time.</param>
+        /// <param name="gclid">The Google Click ID for which adjustments are uploaded.</param>
+        /// <param name="adjustmentDateTime">The adjustment date and time.</param>
+        /// <param name="restatementValue">The restatement value.</param>
+        public void Run(GoogleAdsClient client, long customerId, long conversionActionId,
+            string gclid, string conversionDateTime, string adjustmentDateTime,
+            ConversionAdjustmentType adjustmentType,
+            double? restatementValue)
+        {
+            // Get the ConversionAdjustmentUploadService.
+            ConversionAdjustmentUploadServiceClient conversionAdjustmentUploadService =
+                client.GetService(Services.V2.ConversionAdjustmentUploadService);
+
+            // Associate conversion adjustments with the existing conversion action.
+            // The GCLID should have been uploaded before with a conversion.
+            ConversionAdjustment conversionAdjustment = new ConversionAdjustment()
+            {
+                ConversionAction = ResourceNames.ConversionAction(customerId, conversionActionId),
+                AdjustmentType = adjustmentType,
+                GclidDateTimePair = new GclidDateTimePair()
+                {
+                    Gclid = gclid,
+                    ConversionDateTime = conversionDateTime,
+                },
+                AdjustmentDateTime = adjustmentDateTime,
+            };
+
+            // Set adjusted value for adjustment type RESTATEMENT.
+            if (adjustmentType == ConversionAdjustmentType.Restatement)
+            {
+                conversionAdjustment.RestatementValue = new RestatementValue()
+                {
+                    AdjustedValue = restatementValue
+                };
+            }
+
+            try
+            {
+                // Issue a request to upload the conversion adjustment.
+                UploadConversionAdjustmentsResponse response =
+                    conversionAdjustmentUploadService.UploadConversionAdjustments(
+                        customerId.ToString(), new[] { conversionAdjustment }, true, false);
+
+                ConversionAdjustmentResult result = response.Results[0];
+                // Print the result.
+                Console.WriteLine($"Uploaded conversion adjustment value of" +
+                    $" '{result.ConversionAction}' for Google Click ID " +
+                    $"'{result.GclidDateTimePair.Gclid}'");
+            }
+            catch (GoogleAdsException e)
+            {
+                Console.WriteLine("Failure:");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Failure: {e.Failure}");
+                Console.WriteLine($"Request ID: {e.RequestId}");
+            }
+        }
+    }
+}
