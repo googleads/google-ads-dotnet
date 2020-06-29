c1c3d6ed58ff63a3e8a6219c8419b572a87ab2bb
AndrewMBurke <amwb@google.com>
2020-06-29 11:12:46 -0400

    Addressed comments, reworked parallel loops

--- examples/Reporting/ParallelReportDownload.cs
+++ examples/Reporting/ParallelReportDownload.cs
@@ -16,6 +16,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Google.Ads.GoogleAds.Lib;
 using Google.Ads.GoogleAds.V3.Errors;
@@ -52,10 +53,14 @@ namespace Google.Ads.GoogleAds.Examples.V3
             Console.WriteLine(codeExample.Description);
 
             // The Google Ads customer IDs for which the call is made.
-            string customerIds = "INSERT_CUSTOMER_ID_1_HERE, INSERT_CUSTOMER_ID_2_HERE";
+            long[] customerIds = new long[]
+            {
+                long.Parse("INSERT_CUSTOMER_ID_1_HERE"),
+                long.Parse("INSERT_CUSTOMER_ID_2_HERE")
+            };
 
             // Optional login customer ID if your access to the CIDs is via a manager account.
-            string loginCustomerId = "INSERT_MANAGER_ACCOUNT_ID_HERE";
+            long loginCustomerId = long.Parse("INSERT_MANAGER_ACCOUNT_ID_HERE");
 
             codeExample.Run(new GoogleAdsClient(), customerIds, loginCustomerId);
         }
@@ -80,24 +85,22 @@ namespace Google.Ads.GoogleAds.Examples.V3
         /// <param name="customerIds">The Google Ads customer Id.</param>
         /// <param name="loginCustomerId">Optional login customer ID if your access to the CIDs
         ///     is via a manager account.</param>
-        public void Run(GoogleAdsClient client, string customerIds, string loginCustomerId = null)
+        public void Run(GoogleAdsClient client, long[] customerIds, long? loginCustomerId)
         {
             // If a manager ID is supplied, update the login credentials.
-            if (loginCustomerId != null)
+            if (loginCustomerId.HasValue)
             {
-                client.Config.LoginCustomerId = loginCustomerId;
+                client.Config.LoginCustomerId = loginCustomerId.ToString();
             }
 
             // Get the GoogleAdsService. A single client can be shared by all threads.
             GoogleAdsServiceClient googleAdsService = client.GetService(
                 Services.V3.GoogleAdsService);
 
-            List<long> ids = customerIds.Split(',').Select(long.Parse).ToList();
-
             try
             {
                 // Begin downloading reports and block program termination until complete.
-                Task task = RunDownloadParallelAsync(googleAdsService, ids);
+                Task task = RunDownloadParallelAsync(googleAdsService, customerIds);
                 task.Wait();
             }
             catch (GoogleAdsException e)
@@ -117,7 +120,7 @@ namespace Google.Ads.GoogleAds.Examples.V3
         /// <param name="customerIds">The list of customer IDs from which to request data.</param>
         /// <returns>The asynchronous operation.</returns>
         private async Task RunDownloadParallelAsync(
-            GoogleAdsServiceClient googleAdsService, List<long> customerIds)
+            GoogleAdsServiceClient googleAdsService, long[] customerIds)
         {
             // List of all requests to ensure that we wait for the reports to complete on all
             // customer IDs before proceeding.
@@ -127,27 +130,33 @@ namespace Google.Ads.GoogleAds.Examples.V3
             ConcurrentBag<ReportDownload> responses = new ConcurrentBag<ReportDownload>();
 
             // Helpful identifier for active tasks.
-            int reportId = 1;
+            //int reportId = 1;
 
             // IMPORTANT: You should avoid hitting the same customer ID in parallel. There are rate
             // limits at the customer ID level which are much stricter than limits at the developer
             // token level. Hitting these limits frequently enough will significantly reduce
             // throughput as the client library will automatically retry with exponential back-off
             // before failing the request.
-            foreach (string query in GAQL_QUERY_STRINGS)
-            {
-                Console.WriteLine($"Query: {query}");
+            Parallel.ForEach(GAQL_QUERY_STRINGS, (query, outerState, outerIndex) =>
 
-                foreach (long customerId in customerIds)
+                    // foreach (string query in GAQL_QUERY_STRINGS)
                 {
-                    Console.WriteLine($"\tReport {reportId} requested for CID {customerId}.");
+                    Console.WriteLine($"Query: {query}");
 
-                    // Issue an asynchronous search request and add it to the list of requests
-                    // in progress.
-                    tasks.Add(DownloadReportAsync(googleAdsService, customerId, query,
-                        reportId++, responses));
+                    Parallel.ForEach(customerIds, (customerId, innerState, innerIndex) =>
+                        // foreach (long customerId in customerIds)
+                    {
+                        int reportId = (int) (outerIndex * GAQL_QUERY_STRINGS.Length + innerIndex);
+                        Console.WriteLine($"\tReport {reportId} requested " +
+                            $"for CID {customerId}.");
+
+                        // Issue an asynchronous search request and add it to the list of requests
+                        // in progress.
+                        tasks.Add(DownloadReportAsync(googleAdsService, customerId, query,
+                            reportId, responses));
+                    });
                 }
-            }
+            );
 
             Console.WriteLine($"Awaiting results from {tasks.Count} requests...\n");
 
@@ -184,7 +193,12 @@ namespace Google.Ads.GoogleAds.Examples.V3
                     delegate(SearchGoogleAdsStreamResponse resp)
                     {
                         // Store the results.
-                        responses.Add(new ReportDownload(customerId, reportId, resp));
+                        responses.Add(new ReportDownload()
+                        {
+                            CustomerId = customerId,
+                            ReportId = reportId,
+                            Response = resp
+                        });
                     }
                 );
             }
@@ -213,24 +227,9 @@ namespace Google.Ads.GoogleAds.Examples.V3
         /// </summary>
         private class ReportDownload
         {
-            private long CustomerId { get; }
-            private int ReportId { get; }
-            private SearchGoogleAdsStreamResponse Response { get; }
-
-            /// <summary>
-            /// Initialize a saved report with a specific CID, an integer identifier, and the
-            /// returned response from a request.
-            /// </summary>
-            /// <param name="customerId">The customer ID for whom the report was requested.</param>
-            /// <param name="reportId">Integer to simplify identification of the request.</param>
-            /// <param name="response">The data received from a successful request.</param>
-            public ReportDownload(long customerId, int reportId,
-                SearchGoogleAdsStreamResponse response)
-            {
-                CustomerId = customerId;
-                ReportId = reportId;
-                Response = response;
-            }
+            internal long CustomerId { get; set; }
+            internal long ReportId { get; set; }
+            internal SearchGoogleAdsStreamResponse Response { get; set; }
 
             public override string ToString()
             {
