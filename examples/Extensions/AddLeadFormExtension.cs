1dfe0ebf457187d4f83def8e8528e509b3a32252
AndrewMBurke <amwb@google.com>
2021-04-21 11:05:30 -0400

    Add region tags for AddLeadFormExtension

--- examples/Extensions/AddLeadFormExtension.cs
+++ examples/Extensions/AddLeadFormExtension.cs
@@ -96,6 +96,7 @@ namespace Google.Ads.GoogleAds.Examples.V6
         /// <param name="campaignId">ID of the campaign to which lead form extensions are
         /// added.</param>
         /// <param name="leadFormAssetResourceName">The lead form asset resource name.</param>
+        // [START AddLeadFormExtension_1]
         private void CreateLeadFormExtension(GoogleAdsClient client, long customerId,
             long campaignId, string leadFormAssetResourceName)
         {
@@ -124,6 +125,7 @@ namespace Google.Ads.GoogleAds.Examples.V6
                     $" '{result.ResourceName}' for campaign ID {campaignId}.");
             }
         }
+        // [END AddLeadFormExtension_1]
 
         /// <summary>
         /// Creates the lead form asset.
@@ -131,6 +133,7 @@ namespace Google.Ads.GoogleAds.Examples.V6
         /// <param name="client">The Google Ads client.</param>
         /// <param name="customerId">The customer ID for which the call is made.</param>
         /// <returns>The lead form asset resource name.</returns>
+        // [START AddLeadFormExtension]
         private string CreateLeadFormAsset(GoogleAdsClient client, long customerId)
         {
             AssetServiceClient assetService = client.GetService(Services.V6.AssetService);
@@ -230,5 +233,6 @@ namespace Google.Ads.GoogleAds.Examples.V6
                 "was created.");
             return leadFormAssetResourceName;
         }
+        // [END AddLeadFormExtension]
     }
 }
