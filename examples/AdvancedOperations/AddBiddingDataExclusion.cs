6f2634641df732d4ff484ca9f63f82c713af9e64
AnashOommen <adwordsapiadvisor+anash@google.com>
2021-09-29 12:18:03 -0400

    Add new code examples for bidding data exclusion and seasonality adjustments

---
+++ examples/AdvancedOperations/AddBiddingDataExclusion.cs
@@ -0,0 +1,161 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using CommandLine;
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.V8.Errors;
+using Google.Ads.GoogleAds.V8.Resources;
+using Google.Ads.GoogleAds.V8.Services;
+using System;
+using System.Collections.Generic;
+using static Google.Ads.GoogleAds.V8.Enums.AdvertisingChannelTypeEnum.Types;
+using static Google.Ads.GoogleAds.V8.Enums.SeasonalityEventScopeEnum.Types;
+
+namespace Google.Ads.GoogleAds.Examples.V8
+{
+    /// <summary>
+    /// This code example adds a channel-level data exclusion that excludes conversions from
+    /// being used by Smart Bidding for the time interval specified.
+    ///
+    /// For more information on using data exclusions, see
+    /// https://developers.google.com/google-ads/api/docs/campaigns/bidding/data-exclusions.
+    /// </summary>
+    public class AddBiddingDataExclusion : ExampleBase
+    {
+        /// <summary>
+        /// Command line options for running the <see cref="AddBiddingDataExclusion"/> example.
+        /// </summary>
+        public class Options : OptionsBase
+        {
+            /// <summary>
+            /// The Google Ads customer ID for which the call is made.
+            /// </summary>
+            [Option("customerId", Required = true, HelpText =
+                "The Google Ads customer ID for which the call is made.")]
+            public long CustomerId { get; set; }
+
+            /// <summary>
+            /// The start date time in yyyy-MM-dd HH:mm:ss format of the data exclusion period.
+            /// </summary>
+            [Option("startDateTime", Required = true, HelpText =
+                "The start date time in yyyy-MM-dd HH:mm:ss format of the data exclusion period.")]
+            public string StartDateTime { get; set; }
+
+            /// <summary>
+            /// The end date time in yyyy-MM-dd HH:mm:ss format of the data exclusion period.
+            /// </summary>
+            [Option("endDateTime", Required = true, HelpText =
+                "The end date time in yyyy-MM-dd HH:mm:ss format of the data exclusion period.")]
+            public string EndDateTime { get; set; }
+        }
+
+        /// <summary>
+        /// Main method, to run this code example as a standalone application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            Options options = new Options();
+            CommandLine.Parser.Default.ParseArguments<Options>(args).MapResult(
+                delegate (Options o)
+                {
+                    options = o;
+                    return 0;
+                }, delegate (IEnumerable<Error> errors)
+                {
+                    // The Google Ads customer ID for which the call is made.
+                    options.CustomerId = long.Parse("INSERT_CUSTOMER_ID_HERE");
+
+                    // The start date time in yyyy-MM-dd HH:mm:ss format of the data exclusion
+                    // period.
+                    options.StartDateTime = "INSERT_START_DATE_TIME_HERE";
+
+                    // The end date time in yyyy-MM-dd HH:mm:ss format of the data exclusion
+                    // period.
+                    options.EndDateTime = "INSERT_END_DATE_TIME_HERE";
+
+                    return 0;
+                });
+
+            AddBiddingDataExclusion codeExample = new AddBiddingDataExclusion();
+            Console.WriteLine(codeExample.Description);
+            codeExample.Run(new GoogleAdsClient(), options.CustomerId, options.StartDateTime,
+                options.EndDateTime);
+        }
+
+        /// <summary>
+        /// Returns a description about the code example.
+        /// </summary>
+        public override string Description =>
+            "This code example adds a channel-level data exclusion that excludes conversions " +
+            "from being used by Smart Bidding for the time interval specified. For more " +
+            "information on using data exclusions, see " +
+            "https://developers.google.com/google-ads/api/docs/campaigns/bidding/data-exclusions.";
+
+        /// <summary>
+        /// Runs the code example.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for which the call is made.</param>
+        /// <param name="startDateTime">The start time of the data exclusion (in
+        /// yyyy-MM-dd HH:mm:ss format) in the account's timezone.</param>
+        /// <param name="endDateTime">The end time of the data exclusion (in
+        /// yyyy-MM-dd HH:mm:ss format) in the account's timezone.</param>
+        public void Run(GoogleAdsClient client, long customerId, string startDateTime,
+            string endDateTime)
+        {
+            // Get the BiddingDataExclusionServiceClient.
+            BiddingDataExclusionServiceClient biddingDataExclusionService =
+                client.GetService(Services.V8.BiddingDataExclusionService);
+
+            // [START add_bidding_data_exclusion]
+            BiddingDataExclusion dataExclusion = new BiddingDataExclusion()
+            {
+                // A unique name is required for every data exclusion.
+                Name = "Data exclusion #" + ExampleUtilities.GetRandomString(),
+                // The CHANNEL scope applies the data exclusion to all campaigns of specific
+                // advertising channel types. In this example, the the exclusion will only apply to
+                // Search campaigns. Use the CAMPAIGN scope to instead limit the scope to specific
+                // campaigns.
+                Scope = SeasonalityEventScope.Channel,
+                AdvertisingChannelTypes = { AdvertisingChannelType.Search },
+                // The date range should be less than 14 days.
+                StartDateTime = startDateTime,
+                EndDateTime = endDateTime,
+            };
+            BiddingDataExclusionOperation operation = new BiddingDataExclusionOperation()
+            {
+                Create = dataExclusion
+            };
+
+            try
+            {
+                MutateBiddingDataExclusionsResponse response =
+                    biddingDataExclusionService.MutateBiddingDataExclusions(
+                        customerId.ToString(), new[] { operation });
+                Console.WriteLine($"Added data exclusion with resource name: " +
+                    $"{response.Results[0].ResourceName}");
+            }
+            catch (GoogleAdsException e)
+            {
+                Console.WriteLine("Failure:");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Failure: {e.Failure}");
+                Console.WriteLine($"Request ID: {e.RequestId}");
+                throw;
+            }
+            // [END add_bidding_data_exclusion]
+        }
+    }
+}
---
+++ examples/AdvancedOperations/AddBiddingSeasonalityAdjustment.cs
@@ -0,0 +1,192 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using CommandLine;
+using Google.Ads.GoogleAds.Lib;
+using Google.Ads.GoogleAds.V8.Errors;
+using Google.Ads.GoogleAds.V8.Resources;
+using Google.Ads.GoogleAds.V8.Services;
+using System;
+using System.Collections.Generic;
+using static Google.Ads.GoogleAds.V8.Enums.AdvertisingChannelTypeEnum.Types;
+using static Google.Ads.GoogleAds.V8.Enums.SeasonalityEventScopeEnum.Types;
+
+namespace Google.Ads.GoogleAds.Examples.V8
+{
+    /// <summary>
+    /// This code example adds a channel-level seasonality adjustment that adjusts Smart Bidding
+    /// behavior by the expected change in conversion rate for the given future time interval.
+    ///
+    /// For more information on using seasonality adjustments, see
+    /// https://developers.google.com/google-ads/api/docs/campaigns/bidding/seasonality-adjustments.
+    /// </summary>
+    public class AddBiddingSeasonalityAdjustment : ExampleBase
+    {
+        /// <summary>
+        /// Command line options for running the <see cref="AddBiddingSeasonalityAdjustment"/> example.
+        /// </summary>
+        public class Options : OptionsBase
+        {
+            /// <summary>
+            /// The Google Ads customer ID for which the call is made.
+            /// </summary>
+            [Option("customerId", Required = true, HelpText =
+                "The Google Ads customer ID for which the call is made.")]
+            public long CustomerId { get; set; }
+
+            /// <summary>
+            /// The start time of the seasonality adjustment (in yyyy-MM-dd HH:mm:ss format) in
+            /// the account's timezone.
+            /// </summary>
+            [Option("startDateTime", Required = true, HelpText =
+                "The start time of the seasonality adjustment (in yyyy-MM-dd HH:mm:ss format) " +
+                "in the account's timezone.")]
+            public string StartDateTime { get; set; }
+
+            /// <summary>
+            /// The end time of the seasonality adjustment (in yyyy-MM-dd HH:mm:ss format) in
+            /// the account's timezone.
+            /// </summary>
+            [Option("endDateTime", Required = true, HelpText =
+                "The end time of the seasonality adjustment (in yyyy-MM-dd HH:mm:ss format) in " +
+                "the account's timezone.")]
+            public string EndDateTime { get; set; }
+
+            /// <summary>
+            /// The conversion rate adjustment (an increase or a decrease), which accounts for
+            /// estimated changes in conversion rate due to a future event.
+            /// </summary>
+            [Option("conversionRateModifier", Required = true, HelpText =
+                "The conversion rate adjustment (an increase or a decrease), which accounts for " +
+                "estimated changes in conversion rate due to a future event.")]
+            public double ConversionRateModifier { get; set; }
+        }
+
+        /// <summary>
+        /// Main method, to run this code example as a standalone application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            Options options = new Options();
+            CommandLine.Parser.Default.ParseArguments<Options>(args).MapResult(
+                delegate (Options o)
+                {
+                    options = o;
+                    return 0;
+                }, delegate (IEnumerable<Error> errors)
+                {
+                    // The Google Ads customer ID for which the call is made.
+                    options.CustomerId = long.Parse("INSERT_CUSTOMER_ID_HERE");
+
+                    // The start time of the seasonality adjustment (in
+                    // yyyy-MM-dd HH:mm:ss format) in the account's timezone.")]
+                    options.StartDateTime = "INSERT_START_DATE_TIME_HERE";
+
+                    // The end time of the seasonality adjustment (in
+                    // yyyy-MM-dd HH:mm:ss format) in the account's timezone.")]
+                    options.EndDateTime = "INSERT_END_DATE_TIME_HERE";
+
+                    options.ConversionRateModifier =
+                        long.Parse("INSERT_CONVERSION_RATE_MODIFIER_HERE");
+
+                    return 0;
+                });
+
+            AddBiddingSeasonalityAdjustment codeExample = new AddBiddingSeasonalityAdjustment();
+            Console.WriteLine(codeExample.Description);
+            codeExample.Run(new GoogleAdsClient(), options.CustomerId, options.StartDateTime,
+                options.EndDateTime, options.ConversionRateModifier);
+        }
+
+        /// <summary>
+        /// Returns a description about the code example.
+        /// </summary>
+        public override string Description =>
+            "This code example adds a channel-level seasonality adjustment that adjusts Smart " +
+            "Bidding behavior by the expected change in conversion rate for the given future " +
+            "time interval. For more information on using seasonality adjustments, see " +
+            "https://developers.google.com/google-ads/api/docs/campaigns/bidding/seasonality-adjustments.";
+
+        /// <summary>
+        /// Runs the code example.
+        /// </summary>
+        /// <param name="client">The Google Ads client.</param>
+        /// <param name="customerId">The Google Ads customer ID for which the call is made.</param>
+        /// <param name="startDateTime">The start time of the data exclusion (in
+        /// yyyy-MM-dd HH:mm:ss format) in the account's timezone.</param>
+        /// <param name="endDateTime">The end time of the data exclusion (in
+        /// yyyy-MM-dd HH:mm:ss format) in the account's timezone.</param>
+        /// <param name="conversionRateModifier">The conversion rate adjustment (an increase or a
+        /// decrease), which accounts for estimated changes in conversion rate due to a future
+        /// event.</param>
+        public void Run(GoogleAdsClient client, long customerId, string startDateTime,
+            string endDateTime, double conversionRateModifier)
+        {
+            // Get the BiddingSeasonalityAdjustmentServiceClient.
+            BiddingSeasonalityAdjustmentServiceClient biddingSeasonalityAdjustmentService =
+                client.GetService(Services.V8.BiddingSeasonalityAdjustmentService);
+
+
+            // [START add_bidding_seasonality_adjustment]
+            BiddingSeasonalityAdjustment seasonalityAdjustment =
+                new BiddingSeasonalityAdjustment()
+                {
+                    // A unique name is required for every seasonality adjustment.
+                    Name = "Seasonality adjustment #" + ExampleUtilities.GetRandomString(),
+                    // The CHANNEL scope applies the conversionRateModifier to all campaigns of
+                    // specific advertising channel types. In this example, the
+                    // conversionRateModifier will only apply to Search campaigns. Use the
+                    // CAMPAIGN scope to instead limit the scope to specific campaigns.
+                    Scope = SeasonalityEventScope.Channel,
+                    AdvertisingChannelTypes = { AdvertisingChannelType.Search },
+                    // If setting scope CAMPAIGN, add individual campaign resource name(s)
+                    // according to the commented out line below.
+                    // Campaigns = { "INSERT_CAMPAIGN_RESOURCE_NAME_HERE" },
+                    // The date range should be less than 14 days.
+                    StartDateTime = startDateTime,
+                    EndDateTime = endDateTime,
+                    // The conversionRateModifier is the expected future conversion rate change.
+                    // When this field is unset or set to 1.0, no adjustment will be applied to
+                    // traffic. The allowed range is 0.1 to 10.0.
+                    ConversionRateModifier = conversionRateModifier
+                };
+
+            BiddingSeasonalityAdjustmentOperation operation =
+                new BiddingSeasonalityAdjustmentOperation()
+                {
+                    Create = seasonalityAdjustment
+                };
+
+            try
+            {
+                MutateBiddingSeasonalityAdjustmentsResponse response =
+                    biddingSeasonalityAdjustmentService.MutateBiddingSeasonalityAdjustments(
+                        customerId.ToString(), new[] { operation });
+                Console.WriteLine($"Added seasonality adjustment with resource name: " +
+                    $"{response.Results[0].ResourceName}");
+            }
+            catch (GoogleAdsException e)
+            {
+                Console.WriteLine("Failure:");
+                Console.WriteLine($"Message: {e.Message}");
+                Console.WriteLine($"Failure: {e.Failure}");
+                Console.WriteLine($"Request ID: {e.RequestId}");
+                throw;
+            }
+
+            // [END add_bidding_seasonality_adjustment]
+        }
+    }
+}
