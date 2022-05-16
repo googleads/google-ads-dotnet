54b445f8977b678f8bcbfb7ef0ed499611d1409b
Anash P. Oommen <AnashOommen@users.noreply.github.com>
2022-05-16 12:53:57 -0400

    Fix an NPE when initalizing GoogleAdsException without metadata. Fixes https://github.com/googleads/google-ads-dotnet/issues/402 (#404)

--- Google.Ads.Gax/src/Lib/AdsBaseException.cs
+++ Google.Ads.Gax/src/Lib/AdsBaseException.cs
@@ -93,7 +93,12 @@ namespace Google.Ads.Gax.Lib
         /// <param name="trailers">Response trailing metadata.</param>
         protected AdsBaseException(Status status, Metadata trailers) : base(status, trailers)
         {
-            Failure = ProtobufUtilities.Parse<T>(trailers.GetEntry(FailureKey).ValueBytes);
+            Metadata.Entry failureData = trailers.GetEntry(FailureKey);
+            if (failureData != null && failureData.ValueBytes != null)
+            {
+                Failure = ProtobufUtilities.Parse<T>(failureData.ValueBytes);
+            }
+                
             // Try to extract the request ID from the response trailers, and fall back
             // to extract from Failure if possible.
             this.RequestId = new AdsResponseMetadata(trailers).RequestId;
