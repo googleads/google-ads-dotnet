c3bd7cb47f771c02e0b0d71c8528c3c24954e355
Anash Oommen <anash@google.com>
2026-08-07 10:56:42 -0400

    Release v25
    FolderOrigin-RevId: /tmpfs/src/git/google-ads-dotnet

--- Google.Ads.GoogleAds/tests/Lib/GoogleAdsFailureExtensionTests.cs
+++ Google.Ads.GoogleAds/tests/Lib/GoogleAdsFailureExtensionTests.cs
@@ -23,10 +23,10 @@ namespace Google.Ads.GoogleAds.Tests.Lib
     /// <summary>
     /// UnitTests for extensions in GoogleAdsFailureExtensionsTests.cs.
     /// </summary>
+    [TestFixture("21")]
     [TestFixture("22")]
     [TestFixture("23")]
     [TestFixture("24")]
-    [TestFixture("25")]
     internal class GoogleAdsFailureExtensionsTests
     {
         private string apiVersion;
