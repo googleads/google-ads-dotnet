2ca2744152d1ac434c6c21956c492f70fc87a01a
Anash Oommen <anash@google.com>
2026-08-13 17:37:54 -0400

    Release v25
    FolderOrigin-RevId: /tmpfs/src/git/google-ads-dotnet

--- Google.Ads.Gax/src/Config/AdsConfig.cs
+++ Google.Ads.Gax/src/Config/AdsConfig.cs
@@ -1,4 +1,4 @@
-// Copyright 2022 Google LLC
+﻿// Copyright 2022 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -319,11 +319,7 @@ namespace Google.Ads.Gax.Config
         public bool UseApplicationDefaultCredentials
         {
             get => useApplicationDefaultCredentials.Value;
-            set
-            {
-                SetPropertyAndNotify(useApplicationDefaultCredentials, value);
-                credential = null;
-            }
+            set => SetPropertyAndNotify(useApplicationDefaultCredentials, value);
         }
 
         /// <summary>
@@ -575,7 +571,7 @@ namespace Google.Ads.Gax.Config
         private ICredential credential = null;
 
         /// <summary>
-        /// Gets or sets the credentials.
+        /// Gets the credentials.
         /// </summary>
         public ICredential Credentials
         {
@@ -587,7 +583,6 @@ namespace Google.Ads.Gax.Config
                 }
                 return credential;
             }
-            set => credential = value;
         }
 
         /// <summary>
